cf43d066bcdf55b3c7a26b829b5a5275a75a2372
Thomas joseph <tj089502@gmail.com>
2023-12-09 20:46:58 +0530

    prototype / prototypeinheritance

---
+++ prototype.js
@@ -0,0 +1,11 @@
+const obj={
+    name:"somethimg"
+}
+
+let arr=[2,4,5,4]
+
+//about prototype i think the saying everthing in javascript is an obj is right
+
+console.log(obj.__proto__);
+console.log(arr.__proto__.__proto__);
+
