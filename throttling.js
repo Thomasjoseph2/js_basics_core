1e94338a2197010f5e74fae19be72c7575d82e32
Thomas joseph <tj089502@gmail.com>
2023-12-09 19:43:52 +0530

    throttling

--- throttling.js
+++ throttling.js
@@ -2,8 +2,6 @@ const apicall = () => {
   console.log("api call...");
 };
 
-
-
 const throttler = function throttle(apicall, limit) {
   let timer;
   return function () {
@@ -16,7 +14,35 @@ const throttler = function throttle(apicall, limit) {
   };
 };
 
+//const betterThrottler=throttler(apicall, 400)
 
-const betterThrottler=throttler(apicall, 400)
+//window.addEventListener("resize", betterThrottler);
+
+//another logic to impliment
+
+const betterOne = function flagger(apicall, limit) {
+  let flag = true;
+  let timer;
+  return function () {
+    if (flag) {
+      apicall();
+      flag = false;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        flag = true;
+      }, limit);
+    }
+  };
+};
+const betterThrottler = betterOne(apicall, 1000);
 
 window.addEventListener("resize", betterThrottler);
+
+//top one is better we are managing the this there  we can also do that in the second one also im not doing it now bcz im lazy  
+
+
+
+
+
+
+
