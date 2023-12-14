844f9630cf3f3ae3c3d23e2c63c02e3975d2185d
Thomas joseph <tj089502@gmail.com>
2023-12-14 11:53:48 +0530

    recursive

--- recurssivethink.js
+++ recurssivethink.js
@@ -6,41 +6,38 @@
 //etc...
 //}
 
-let user={
-    name:'thomas',
-    address:{
-        personal:{
-            city:'kottayam',
-            area:'west'
+let user = {
+  name: "thomas",
+  address: {
+    personal: {
+      city: "kottayam",
+      area: "west",
+    },
+    office: {
+      city: "ekm",
+      area: {
+        landmark: "okok",
+        pin: 5656,
+        obj: {
+          key: "value",
         },
-        office:{
-            city:'ekm',
-            area:{
-                landmark:'okok',
-                pin:5656,
-                obj:{
-                  key:'value'
-                }
-            }
-        }
+      },
+    },
+  },
+};
+
+let findobj = {};
+
+function recurr(obj, parent) {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      recurr(obj[key], parent + "_" + key);
+    } else {
+      findobj[parent + "_" + key] = obj[key];
     }
+  }
 }
 
-let findobj={}
-
-function recurr( obj,parent){
-
-    for (let key in obj){
-        if(typeof(obj[key])==="object"){
-            recurr(obj[key],parent+"_"+key)
-        }else{
-            findobj[parent+'_'+key]=obj[key]
-        }
-    }
-
-
-}
-
-recurr(user,"user")
+recurr(user, "user");
 
 console.log(findobj);
