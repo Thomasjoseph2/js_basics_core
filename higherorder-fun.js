53ef74f6ef26968e9d7cc8f89c6484fdb05ce18e
Thomas joseph <tj089502@gmail.com>
2023-12-14 16:10:28 +0530

    create own function and add it to prototype

--- higherorder-fun.js
+++ higherorder-fun.js
@@ -1,30 +1,38 @@
+const radius = [10, 20, 30, 50];
 
+const area = (radius) => {
+  return Math.PI * (radius * radius);
+};
 
+const circumference = (radius) => {
+  return 2 * Math.PI * radius;
+};
 
-const radius=[10,20,30,50]
+const diameter = (radius) => {
+  return 2 * radius;
+};
 
-const area=(radius)=>{
-    return Math.PI * (radius * radius);
+const calculate = (radius, logic) => {
+  const output = radius.map((r) => {
+    return logic(r);
+  });
+  return output;
+};
+
+//map like function
+Array.prototype.dupeMap=function (logic){
+  const output=[]
+  for(let i=0;i<this.length;i++){
+    output.push(logic(this[i]))
+  }
+  return output
 }
 
-const circumference=(radius)=>{
-    return (2*Math.PI*radius)
-}
+//realization point map and calculate looks the same then why bother creating calculate?
+//and we can create map
 
-const diameter=(radius)=>{
-    return 2*radius
-}
-
-const calculate =(radius,logic)=>{
-    const output=radius.map((r)=>{
-      return logic(r);
-    })
-   return output
-}
-
-
-
-
-console.log(calculate(radius,area));
-console.log(calculate(radius,circumference));
-console.log(calculate(radius,diameter));
+console.log(radius.map(area));
+console.log(radius.dupeMap(area));
+console.log(calculate(radius, area));
+// console.log(calculate(radius,circumference));
+// console.log(calculate(radius,diameter));
